c07342f22ec560f35c38bbc2f8d1cdb2e18f61fa
Matt Wilkinson <mattwilki17@gmail.com>
2020-11-09 00:04:16 -0500

    pug chat

--- routes.js
+++ routes.js
@@ -75,7 +75,7 @@ module.exports = function (app, myDataBase) {
     );
 
   app.route("/chat").get(ensureAuthenticated, (req, res) => {
-    res.render("chat", { user: req.user });
+    res.render("pug/chat", { user: req.user });
   });
 
   app.use((req, res, next) => {
