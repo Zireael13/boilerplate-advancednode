a8b7bae0cbed00b555bfe3e078c87db584fa6ca8
Matt Wilkinson <mattwilki17@gmail.com>
2020-11-08 23:33:02 -0500

    http io

--- routes.js
+++ routes.js
@@ -63,14 +63,20 @@ module.exports = function (app, myDataBase) {
   );
 
   app.route("/auth/github").get(passport.authenticate("github"));
+
   app
     .route("/auth/github/callback")
     .get(
       passport.authenticate("github", { failureRedirect: "/" }),
       (req, res) => {
-        res.redirect("/profile");
+        req.session.user_id = req.user.id;
+        res.redirect("/chat");
       }
     );
+
+  app.route("/chat").get(ensureAuthenticated, (req, res) => {
+    res.render("pug/chat", { user: req.user });
+  });
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-"use strict";
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const myDB = require("./connection");
 const routes = require("./routes");
@@ -30,6 +31,10 @@ app.use(passport.session());
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
 
+  io.on("connection", (socket) => {
+    console.log("A user has connected");
+  });
+
   routes(app, myDataBase);
   auth(app, myDataBase);
 }).catch((e) => {
@@ -38,6 +43,6 @@ myDB(async (client) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
