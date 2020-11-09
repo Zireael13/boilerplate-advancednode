c39cf56711eda1ea59f0ce9f0767732fc3bc9fd1
Matt Wilkinson <mattwilki17@gmail.com>
2020-11-08 23:13:14 -0500

    auth

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
-const myDB = require("./connection");
+const session = require("express-session");
+const passport = require("passport");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const myDB = require("./connection");
+const routes = require("./routes");
+const auth = require("./auth");
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
+fccTesting(app); // For FCC testing purposes
+app.use("/public", express.static(`${process.cwd()}/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
-app.use("view engine", "pug");
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.route("/").get((req, res) => {
-  res.render("pug", {
-    message: "Please login",
-    title: "Hello",
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
   });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  console.log(`Listening on port ${process.env.PORT}`);
 });
