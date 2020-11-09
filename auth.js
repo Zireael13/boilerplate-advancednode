5646e1049bb514feef7cada37b37cda83add8bab
Matt Wilkinson <mattwilki17@gmail.com>
2020-11-09 00:27:57 -0500

    eslint

--- auth.js
+++ auth.js
@@ -1,7 +1,8 @@
+/* eslint-disable */
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
-const { ObjectID } = require("mongodb");
+const ObjectID = require("mongodb").ObjectID;
 const GitHubStrategy = require("passport-github").Strategy;
 
 module.exports = function (app, myDataBase) {
@@ -16,8 +17,8 @@ module.exports = function (app, myDataBase) {
   });
   passport.use(
     new LocalStrategy(function (username, password, done) {
-      myDataBase.findOne({ username }, function (err, user) {
-        console.log(`User ${username} attempted to log in.`);
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log("User " + username + " attempted to log in.");
         if (err) {
           return done(err);
         }
@@ -42,32 +43,7 @@ module.exports = function (app, myDataBase) {
       },
       function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
-        myDataBase.findAndModify(
-          { id: profile.id },
-          {},
-          {
-            $setOnInsert: {
-              id: profile.id,
-              name: profile.displayName || "John Doe",
-              photo: profile.photos[0].value || "",
-              email: Array.isArray(profile.emails)
-                ? profile.emails[0].value
-                : "No public email",
-              created_on: new Date(),
-              provider: profile.provider || "",
-            },
-            $set: {
-              last_login: new Date(),
-            },
-            $inc: {
-              login_count: 1,
-            },
-          },
-          { upsert: true, new: true },
-          (err, doc) => {
-            return cb(null, doc.value);
-          }
-        );
+        // Database logic here with callback containing our user object
       }
     )
   );
